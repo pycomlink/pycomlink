e4b53af41312ab3044914a6b21cd9d8af923619b
Christian Chwala <christian.chwala@kit.edu>
2014-12-02 13:19:01 +0000

    Test module structur

---
+++ pycomlink/comlink.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  2 11:18:22 2014
+
+@author: chwala-c
+"""
+
+from __future__ import division
+import numpy as np
+
+class Comlink():
+    """Object for commercial microwave link data processing"""
+    def __init__(self):
+        pass
+    def plot(self):
+        pass
+    

7dcf9b6e08577d02c66728ae9b44cbc870b42f84
Christian Chwala <christian.chwala@kit.edu>
2015-03-25 20:28:28 +0100

    Added stuff for pypi packaging and removed unnecessary data

---
+++ README.rst
@@ -0,0 +1,31 @@
+=========
+pycomlink
+=========
+
+A python toolbox for MW link data processing and rain rate estimation
+
+Installation
+------------
+
+`pycomlink` works with Python 2.7 and depends on `numpy`, `pandas`, `matplotlib` and `matplotlib`'s `basemap`. You can install it via pip:
+
+    $ pip install pycomlink
+
+    Usage
+    -----
+
+    Here is an IPython notebook showing the `basic workflow <http://nbviewer.ipython.org/urls/bitbucket.org/cchwala/pycomlink/raw/53cdca3ca142b91d40d2ba5686f68d7659a3c7e4/notebooks/example_workflow.ipynb>`_
+
+    Status
+    ------
+    The basic functionality is already working. However, `pycomlink` is currently in ongoing development. Hence, there may be changes to the interface in the future.
+
+    Features
+    --------
+     * Easily parse your MW link data and use and object oriented approach to do all the processing
+      * Two wet/dry classification methods available
+       * Different baseline methods available
+        * One wet antenna estimation method available
+         * 2D plots using IDW (preliminary version) 
+
+
--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ Created on Tue Dec  2 13:20:35 2014
 @author: chwala-c
 """
 
-import os
+#import os
 from setuptools import setup
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+#def read(fname):
+#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "pycomlink",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Christian Chwala",
     author_email = "christian.chwala@kit.edu",
     description = ("Python tools for MW link data processing"),
@@ -25,12 +25,19 @@ setup(
     keywords = "microwave links precipitation radar",
     url = "https://bitbucket.org/cchwala/pycomlink",
     packages=['pycomlink'],
-    long_description=read('README.md'),
+    #long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
         "License :: OSI Approved :: BSD",
+        'Programming Language :: Python :: 2.7',
     ],
     # A list of all available classifiers can be found at 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    install_requires=[
+        'numpy', 
+        'scipy', 
+        'pandas', 
+        'matplotlib', 
+        'basemap'],
 )

dea14ec9811b602645ed8b8574ca64f947e22ff4
Christian Chwala <christian.chwala@kit.edu>
2017-07-27 11:54:40 +0200

    ENH: Added notebook for baseline determination and example data loader

--- pycomlink/io/__init__.py
+++ pycomlink/io/__init__.py
@@ -1,2 +1,4 @@
 from .cmlh5 import write_to_cmlh5, read_from_cmlh5
 from .csv import write_to_wasim_input_file
+
+import examples
---
+++ pycomlink/io/examples.py
@@ -0,0 +1,15 @@
+import pkg_resources
+from os import path
+
+from pycomlink.io.cmlh5 import read_from_cmlh5
+
+
+def get_example_data_path():
+    return pkg_resources.resource_filename('pycomlink',
+                                           '../notebooks/example_data')
+
+
+def read_one_cml():
+    data_path = get_example_data_path()
+    fn = 'one_cml.h5'
+    return read_from_cmlh5(path.join(data_path, fn))[0]

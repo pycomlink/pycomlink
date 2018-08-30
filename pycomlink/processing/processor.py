Fix Ar_max calculation to fill NaNs until window can be filled
Fix Ar_max calculation to fill NaNs in the beginning of the series until a full window can be filled.

pycomlink/processing/processor.py
@@ -146,9 +146,10 @@ def _calc_A_min_max(tx_min, tx_max, rx_min, rx_max, gT=1.0, gR=0.6, window=7):
     Ac_min[np.isnan(Ac_min)] = np.rint(np.nanmean(Ac_min))
 
     # zero-level calculation
-    Ar_max = np.full(Ac_max.shape, 0)
-    for i in range(len(Ac_max)):
-        Ar_max[i] = Ac_max[i] - Ac_min[max(0, i-window):i+1].min()
+    Ar_max = np.full(Ac_max.shape, np.nan)
+    for i in range(window,len(Ac_max)):
+        Ar_max[i] = Ac_max[i] - Ac_min[i-window:i+1].min()
+    Ar_max[0:window] = np.nan
 
     return Ar_max
 
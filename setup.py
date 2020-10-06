bd15ed4dd55fb2735578b18194bb3e9966cb40d9
Christian Chwala <christian.chwala@kit.edu>
2020-10-07 00:01:32 +0200

    bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name = "pycomlink",
-    version = "0.2.4",
+    version = "0.2.5",
     author = "Christian Chwala",
     author_email = "christian.chwala@kit.edu",
     description = ("Python tools for MW link data processing"),

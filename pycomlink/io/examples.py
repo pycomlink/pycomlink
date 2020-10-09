8e0165c8cc094c3e6cbdda4960016d1c4e00a58b
Nico Blettner <nico.blettner@kit.edu>
2020-10-09 11:42:11 +0200

    Scripts formatted using black

--- pycomlink/io/examples.py
+++ pycomlink/io/examples.py
@@ -8,29 +8,29 @@ from pycomlink.io.cmlh5 import read_from_cmlh5
 
 
 def get_example_data_path():
-    return pkg_resources.resource_filename('pycomlink', 'io/example_data')
+    return pkg_resources.resource_filename("pycomlink", "io/example_data")
 
 
 def read_one_cml():
     data_path = get_example_data_path()
-    fn = 'one_cml.h5'
+    fn = "one_cml.h5"
     return read_from_cmlh5(path.join(data_path, fn))[0]
 
 
-@deprecated('This function will be depreciated. Use `get_75_cmls() instead.')
+@deprecated("This function will be depreciated. Use `get_75_cmls() instead.")
 def read_cml_list():
     data_path = get_example_data_path()
-    fn = '75_cmls.h5'
+    fn = "75_cmls.h5"
     return read_from_cmlh5(path.join(data_path, fn))
 
 
 def get_75_cmls():
     data_path = get_example_data_path()
-    fn = '75_cmls.h5'
+    fn = "75_cmls.h5"
     return read_from_cmlh5(path.join(data_path, fn))
 
 
 def get_75_cmls_min_max():
     data_path = get_example_data_path()
-    fn = '75_cmls_min_max.h5'
+    fn = "75_cmls_min_max.h5"
     return read_from_cmlh5(path.join(data_path, fn))
--- pycomlink/tests/test_comlink.py
+++ pycomlink/tests/test_comlink.py
@@ -9,44 +9,41 @@ import pandas as pd
 from pycomlink.core import ComlinkChannel, Comlink
 from pycomlink.tests.test_comlink_channel import assert_comlink_channel_equal
 
-t_date_range = pd.date_range(start='2015-01-01', periods=500, freq='min')
+t_date_range = pd.date_range(start="2015-01-01", periods=500, freq="min")
 t_list = [str(date) for date in t_date_range]
 rx_list = list(np.sin(np.linspace(0, 10, len(t_list))))
 tx_list = list(np.cos(np.linspace(0, 10, len(t_list))))
 
 f = 18.9 * 1e9
 
-df = pd.DataFrame(index=t_date_range, data={'rx': rx_list, 'tx': tx_list})
+df = pd.DataFrame(index=t_date_range, data={"rx": rx_list, "tx": tx_list})
 cml_ch = ComlinkChannel(data=df, frequency=f)
-cml_ch2 = ComlinkChannel(data=df*2, frequency=f*2)
+cml_ch2 = ComlinkChannel(data=df * 2, frequency=f * 2)
 
 
 class TestComlinkInit(unittest.TestCase):
-
     def testWithOneComlinkChannel(self):
         cml = generate_standard_cml()
         np.testing.assert_almost_equal(cml.channel_1.rx, rx_list)
-        assert(cml.channel_1.f_GHz == f / 1e9)
+        assert cml.channel_1.f_GHz == f / 1e9
 
 
 class TestComlinkCopy(unittest.TestCase):
-
     def test_copy(self):
 
         cml = generate_standard_cml()
 
         cml_copy = deepcopy(cml)
 
-        assert(type(cml_copy) == Comlink)
+        assert type(cml_copy) == Comlink
 
         # Test (at least one) metadata attribute
-        assert(cml_copy.metadata['site_a_latitude'] ==
-               cml.metadata['site_a_latitude'])
+        assert cml_copy.metadata["site_a_latitude"] == cml.metadata["site_a_latitude"]
 
         # Test that the new metadata is not a reference but a copy
-        cml.metadata['site_a_latitude'] = 999
-        assert(cml.metadata['site_a_latitude'] == 999)
-        assert(cml_copy.metadata['site_a_latitude'] == 44.1)
+        cml.metadata["site_a_latitude"] = 999
+        assert cml.metadata["site_a_latitude"] == 999
+        assert cml_copy.metadata["site_a_latitude"] == 44.1
 
         # Test that DataFrames of channels are equal
         for ch_name in list(cml.channels.keys()):
@@ -59,20 +56,20 @@ class TestComlinkCopy(unittest.TestCase):
             ch = cml.channels[ch_name]
             ch_copy = cml_copy.channels[ch_name]
             ch.data.rx[1] = -9999
-            assert(ch.rx[1] != ch_copy.rx[1])
+            assert ch.rx[1] != ch_copy.rx[1]
 
             ch_copy.data.rx[2] = -1111
-            assert(ch.rx[2] != ch_copy.rx[2])
+            assert ch.rx[2] != ch_copy.rx[2]
 
-            ch_copy.data['foo'] = 42
-            assert(cml_copy.channels[ch_name].data.foo[0] == 42)
+            ch_copy.data["foo"] = 42
+            assert cml_copy.channels[ch_name].data.foo[0] == 42
 
         cml_copy.process.wet_dry.std_dev(window_length=5, threshold=1)
         # Check that the processing has added a `wet` column to the DataFrame
-        assert('wet' in cml_copy.channel_1.columns)
+        assert "wet" in cml_copy.channel_1.columns
         # Check that the `wet` column does not appear in the data of the
         # original CML
-        assert('wet' not in cml.channel_1.columns)
+        assert "wet" not in cml.channel_1.columns
 
 
 class TestComlinkChannelAppendData(unittest.TestCase):
@@ -92,28 +89,28 @@ class TestComlinkChannelAppendData(unittest.TestCase):
     def test_append_wrong_cml(self):
         cml = generate_standard_cml()
         cml_wrong = generate_standard_cml()
-        cml_wrong.metadata['cml_id'] = 'Wrong_CML_ID'
+        cml_wrong.metadata["cml_id"] = "Wrong_CML_ID"
 
-        self.assertRaises(ValueError,
-                          cml.append_data,
-                          cml_wrong)
+        self.assertRaises(ValueError, cml.append_data, cml_wrong)
 
 
 def generate_standard_cml():
-    cml = Comlink(channels=deepcopy(cml_ch),
-                  metadata={'site_a_latitude': 44.1,
-                            'site_a_longitude': 11.1,
-                            'site_b_latitude': 44.2,
-                            'site_b_longitude': 11.2,
-                            'cml_id': 'foo_bar_123'})
+    cml = Comlink(
+        channels=deepcopy(cml_ch),
+        metadata={
+            "site_a_latitude": 44.1,
+            "site_a_longitude": 11.1,
+            "site_b_latitude": 44.2,
+            "site_b_longitude": 11.2,
+            "cml_id": "foo_bar_123",
+        },
+    )
     return cml
 
 
 def assert_comlink_equal(cml_1, cml_2):
     for key in cml_1.metadata.keys():
-        assert(cml_1.metadata[key] == cml_2.metadata[key])
+        assert cml_1.metadata[key] == cml_2.metadata[key]
 
     for ch_name in cml_1.channels.keys():
-        assert_comlink_channel_equal(cml_1.channels[ch_name],
-                                     cml_2.channels[ch_name])
-
+        assert_comlink_channel_equal(cml_1.channels[ch_name], cml_2.channels[ch_name])

1a9be944fff2d5926baf9b5919414cba9e9fc6ff
max <maximilian.graf@kit.edu>
2020-10-12 16:56:20 +0200

    add minimal test for cmlh5_to_netcdf function

---
+++ pycomlink/tests/test_cmlh5_to_netcdf.py
@@ -0,0 +1,32 @@
+import unittest
+from pycomlink.io.cmlh5 import read_from_cmlh5
+from pycomlink.io.cmlh5_to_netcdf import read_cmlh5_file_to_xarray
+import numpy as np
+import pkg_resources
+
+
+def get_test_data_path():
+    return pkg_resources.resource_filename("pycomlink", "tests/test_data")
+
+
+testdata_fn = str(get_test_data_path() + "/75_cmls_processed.h5")
+
+
+class Testcmlh5tonetcdf(unittest.TestCase):
+    def test_cmlh5_to_netcdf_fun(self):
+
+        # open cmlh5
+        cmlh5_file = read_from_cmlh5(testdata_fn)
+
+        # convert to netcdf
+        cml_xr_file = read_cmlh5_file_to_xarray(testdata_fn)
+
+        # compare h5 data to netcdf data
+        np.testing.assert_array_almost_equal(
+            np.array([13., 14., 14., 15., 15., 15.]),
+            cml_xr_file[19].isel(channel_id=0).tsl.values[1300:1306],
+        )
+        np.testing.assert_array_almost_equal(
+            24.913,
+            cml_xr_file[3].sel(channel_id="channel_1").frequency.values,
+        )

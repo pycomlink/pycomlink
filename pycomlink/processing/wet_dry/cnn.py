8bfce24317ea4cef44a2f7e9e29d2b6ee9149411
Julius Polz <julius.polz@kit.edu>
2020-10-13 16:35:05 +0200

    modified cnn.py to do the same as the gitlab function

--- pycomlink/processing/wet_dry/cnn.py
+++ pycomlink/processing/wet_dry/cnn.py
@@ -1,6 +1,18 @@
-import tensorflow
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.optimizers import SGD
 
-def cnn_wet_dry(trsl_channel_1, trsl_channel_2, threshold):
+
+def _rolling_window(a, window):
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(
+        a, shape=shape, strides=strides, writeable=False
+    )
+
+
+def cnn_wet_dry(trsl_channel_1, trsl_channel_2, threshold, batch_size=100, verbose=0):
     """
     Wet dry classification using the CNN based on channel 1 and channel 2 of a CML
 
@@ -9,7 +21,7 @@ def cnn_wet_dry(trsl_channel_1, trsl_channel_2, threshold):
     trsl_channel_1 : iterable of float
          Time series of received signal level of channel 1
     trsl_channel_2 : iterable of float
-         Time series of received signal level of channel 1
+         Time series of received signal level of channel 2
     threshold : float
          Threshold between 0 and 1 which has to be surpassed to classifiy a period as 'wet'
 
@@ -29,6 +41,60 @@ def cnn_wet_dry(trsl_channel_1, trsl_channel_2, threshold):
     https://doi.org/10.5194/amt-13-3835-2020, 2020.
     """
 
+    # load json and create model
+    json_file = open("cnn_model_files/model_2020.002.180m.json", "r")
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights("cnn_model_files/model_2020.002.180m.h5")
+    print("Loaded model from disk")
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=SGD(lr=0.01, decay=1e-3, momentum=0.9, nesterov=True),
+        metrics=["accuracy"],
+    )
+    #################
+    # Normalization #
+    #################
 
+    df = pd.DataFrame()
+    df["trsl1"] = trsl_channel_1
+    df["trsl2"] = trsl_channel_2
+    idx = df.index
+    df["med1"] = df["trsl1"].rolling(72 * 60, min_periods=2 * 60, center=False).median()
+    df["med2"] = df["trsl2"].rolling(72 * 60, min_periods=2 * 60, center=False).median()
+    df["trsl1"] = df["trsl1"].sub(df["med1"])
+    df["trsl2"] = df["trsl2"].sub(df["med2"])
+    df = df.dropna()
+    df = df.reindex(idx, fill_value=-9999)
 
-    return
+    #########################
+    # generate numpy arrays #
+    #########################
+
+    X_fts = np.moveaxis(
+        np.array(
+            [
+                _rolling_window(df["txrx1"].values, 180),
+                _rolling_window(df["txrx2"].values, 180),
+            ]
+        ),
+        0,
+        -1,
+    )
+    cnn_pred = np.ravel(model.predict(X_fts, batch_size=batch_size, verbose=verbose))
+
+    for i in range(len(cnn_pred)):
+        if (
+                -9999 in df["txrx1"].values[i - 151: i + 31]
+                or -9999 in df["txrx2"].values[i - 151: i + 31]
+        ):
+            cnn_pred[i] = np.nan
+
+    df["prediction"] = np.concatenate(
+        (np.repeat(np.nan, 150), cnn_pred, np.repeat(np.nan, 29)), axis=0
+    )
+
+    df = df.reindex(idx, fill_value=np.nan)
+    return df.prediction.values > threshold

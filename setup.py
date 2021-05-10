0ec35056ddbcb3c2924f5702bd2c61d9eb383ddb
Christian Chwala <christian.chwala@kit.edu>
2021-05-10 22:04:59 +0200

    Read dependencies from one file for setup and travis

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ Created on Tue Dec  2 13:20:35 2014
 import os
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as f:
+    INSTALL_REQUIRES = [rq for rq in f.read().split("\n") if rq != ""]
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -33,23 +36,11 @@ setup(
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
         "License :: OSI Approved :: BSD License",
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     # A list of all available classifiers can be found at 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    install_requires=[
-        'numpy',
-        'scipy',
-        'pandas>=0.22',
-        'matplotlib',
-        'numba',
-        'folium',
-        'h5py',
-        'xarray',
-        'netcdf4',
-        'shapely',
-        'pyproj',
-        'tqdm',
-        'pykrige',
-        'future'],
+    install_requires=INSTALL_REQUIRES,
 )

be7458581f5f4b63241125555a84bf3058345b04
Christian Chwala <christian.chwala@kit.edu>
2021-05-12 20:22:11 +0200

    added docstring to xarray wrapper

--- pycomlink/processing/xarray_wrapper.py
+++ pycomlink/processing/xarray_wrapper.py
@@ -4,6 +4,31 @@ import xarray as xr
 
 
 def xarray_loop_vars_over_dim(vars_to_loop, loop_dim):
+    """
+    A decorator to feed CML data with mutiple channels as xarray.DataArrays to CML processing functions
+
+    Here is an example for how this decorator is used for the WAA Schleiss function::
+
+    @xarray_loop_vars_over_dim(vars_to_loop=["rsl", "baseline", "wet"], loop_dim="channel_id")
+    def waa_schleiss_2013(rsl, baseline, wet, waa_max, delta_t, tau):
+        ...
+
+    Here, `delta_t` and `tau` are not CML data xarray.DataArrays and hence do not
+    have to be looped over.
+
+    Parameters
+    ----------
+
+    vars_to_loop: list of strings
+        List of the names of the variables used as kwargs in the decorated function
+        which should have a dimension `loop_dim` for which the decorated function is
+        then applied individually to each item when looping over `loop_dim`.
+    loop_dim: basestring
+        Name of the dimension which all variables in `vars_to_loop` must have in common
+        and which will be looped over to apply the decorated function.
+
+
+    """
     def decorator(func):
         @wraps(func)
         def inner(*args, **kwargs):

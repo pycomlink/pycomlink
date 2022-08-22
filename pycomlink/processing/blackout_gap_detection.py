c48895a57b53f281afe3642339f69ce66e42cbef
Christian Chwala <christian.chwala@kit.edu>
2022-08-22 11:46:24 +0200

    change dtype in numba function so that it can run with disabled numba for testing

--- pycomlink/processing/blackout_gap_detection.py
+++ pycomlink/processing/blackout_gap_detection.py
@@ -52,7 +52,7 @@ def created_blackout_gap_mask_from_start_end_markers(rsl, gap_start, gap_end):
 
     '''
 
-    mask = np.zeros(rsl.shape, dtype=numba.boolean)
+    mask = np.zeros(rsl.shape, dtype=np.bool_)
     in_blackout_gap = False
     for i in range(len(rsl)):
         if gap_start[i] == True:

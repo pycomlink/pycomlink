c1c6e4ed5744d08396bca65de85a9e4ad37342e1
Christian Chwala <christian.chwala@kit.edu>
2022-08-23 23:56:28 +0200

    forgot to remove the ununsed code in validator module... this is not done in this commit

--- pycomlink/validation/validator.py
+++ pycomlink/validation/validator.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import LineString, Polygon
 
+import pycomlink.spatial.grid_intersection as grid_intersection
 from pycomlink.util.maintenance import deprecated
 
 
@@ -44,7 +45,7 @@ class GridValidator(Validator):
         cml_coords = cml.get_coordinates()
 
         # get intersect weights
-        self.intersect_weights = calc_intersect_weights(
+        self.intersect_weights = grid_intersection.calc_intersect_weights(
             x1_line=cml_coords.lon_a,
             y1_line=cml_coords.lat_a,
             x2_line=cml_coords.lon_b,
@@ -121,7 +122,7 @@ class GridValidator(Validator):
         grid = np.stack(
             [self.xr_ds.longitudes.values, self.xr_ds.latitudes.values], axis=2
         )
-        grid_corners = _calc_grid_corners_for_center_location(grid)
+        grid_corners = grid_intersection._calc_grid_corners_for_center_location(grid)
 
         lons[:-1, :-1] = grid_corners.ll_grid[:, :, 0]
         lons[-1, :-1] = grid_corners.ul_grid[-1, :, 0]
@@ -136,7 +137,7 @@ class GridValidator(Validator):
         cml_coords = cml.get_coordinates()
 
         # get intersect weights
-        intersect, pixel_poly_list = calc_intersect_weights(
+        intersect, pixel_poly_list = grid_intersection.calc_intersect_weights(
             x1_line=cml_coords.lon_a,
             y1_line=cml_coords.lat_a,
             x2_line=cml_coords.lon_b,
@@ -171,228 +172,6 @@ class PointValidator(Validator):
 
     def get_time_series(self, cml, values):
         pass
-
-
-def calc_intersect_weights(
-    x1_line,
-    y1_line,
-    x2_line,
-    y2_line,
-    x_grid,
-    y_grid,
-    grid_point_location="center",
-    offset=None,
-    return_pixel_poly_list=False,
-):
-    """Calculate intersecting weights for a line and a grid
-
-    Calculate the intersecting weights for the line defined by `x1_line`,
-    `y1_line`, `x2_line` and `y2_line` and the grid defined by the x- and y-
-    grid points from `x_grid` and `y_grid`.
-
-    Parameters
-    ----------
-    x1_line : float
-    y1_line : float
-    x2_line : float
-    y2_line : float
-    x_grid : 2D array
-        x-coordinates of grid points
-    y_grid : 2D array
-        y-coordinates of grid points
-    grid_point_location : str, optional
-        The only option currently is `center` which assumes that the
-        coordinates in `xr_ds` represent the centers of grid cells
-    offset : float, optional
-        The offset in units of the coordinates to constrain the calculation
-        of intersection to a bounding box around the CML coordinates. The
-        offset specifies by how much this bounding box will be larger then
-        the width- and height-extent of the CML coordinates.
-    return_pixel_poly_list : bool, optional
-        If `True`, also return the list of shapely.Polygon objects which were
-        used to calculate the intersection weights. Defaults to `False`.
-
-    Returns
-    -------
-
-    intersect : array
-        2D array of intersection weights with shape of the longitudes- and
-        latitudes grid of `xr_ds`
-    pixel_poly_list : list, optional
-        List of shapely.Polygons which were used to calculate intersections
-
-    """
-
-    x_grid = x_grid.astype("float64")
-    y_grid = y_grid.astype("float64")
-
-    # grid = np.stack([xr_ds.longitudes.values, xr_ds.latitudes.values], axis=2)
-    grid = np.stack([x_grid, y_grid], axis=2)
-
-    # Get link coordinates for easy access
-    # cml_coords = cml.get_coordinates()
-
-    # Convert CML to shapely line
-    link = LineString([(x1_line, y1_line), (x2_line, y2_line)])
-
-    # Derive grid cell width to set bounding box offset
-    ll_cell = grid[0, 1, 0] - grid[0, 0, 0]
-    ul_cell = grid[-1, 1, 0] - grid[-1, 0, 0]
-    lr_cell = grid[0, -1, 0] - grid[0, -2, 0]
-    ur_cell = grid[-1, -1, 0] - grid[-1, -2, 0]
-    offset_calc = max(ll_cell, ul_cell, lr_cell, ur_cell)
-
-    # Set bounding box offset
-    if offset is None:
-        offset = offset_calc
-
-    # Set bounding box
-    x_max = max([x1_line, x2_line])
-    x_min = min([x1_line, x2_line])
-    y_max = max([y1_line, y2_line])
-    y_min = min([y1_line, y2_line])
-    # lon_grid = grid[:, :, 0]
-    # lat_grid = grid[:, :, 1]
-    bounding_box = ((x_grid > x_min - offset) & (x_grid < x_max + offset)) & (
-        (y_grid > y_min - offset) & (y_grid < y_max + offset)
-    )
-
-    # Calculate polygon corners assuming that `grid` defines the center
-    # of each grid cell
-    if grid_point_location == "center":
-        grid_corners = _calc_grid_corners_for_center_location(grid)
-    elif grid_point_location == "lower_left":
-        grid_corners = _calc_grid_corners_for_lower_left_location(grid)
-    else:
-        raise ValueError(
-            "`grid_point_location` = %s not implemented" % grid_point_location
-        )
-
-    # Find intersection
-    intersect = np.zeros([grid.shape[0], grid.shape[1]])
-    pixel_poly_list = []
-    # Iterate only over the indices within the bounding box and
-    # calculate the intersect weigh for each pixel
-    ix_in_bbox = np.where(bounding_box == True)
-    for i, j in zip(ix_in_bbox[0], ix_in_bbox[1]):
-        pixel_poly = Polygon(
-            [
-                grid_corners.ll_grid[i, j],
-                grid_corners.lr_grid[i, j],
-                grid_corners.ur_grid[i, j],
-                grid_corners.ul_grid[i, j],
-            ]
-        )
-        pixel_poly_list.append(pixel_poly)
-
-        c = link.intersection(pixel_poly)
-        if not c.is_empty:
-            intersect[i][j] = c.length / link.length
-
-    if return_pixel_poly_list:
-        return intersect, pixel_poly_list
-    else:
-        return intersect
-
-
-def _calc_grid_corners_for_center_location(grid):
-    """
-
-    Parameters
-    ----------
-    grid : array
-        3D matrix holding x and y grids. Shape of `grid` must be
-        (height, width, 2).
-
-    Returns
-    -------
-
-    namedtuple with the grids for the four corners of the grid defined
-    by points at the lower left corner
-
-    """
-
-    grid = grid.astype("float64")
-
-    # Upper right
-    ur_grid = np.zeros_like(grid)
-    ur_grid[0:-1, 0:-1, :] = (grid[0:-1, 0:-1, :] + grid[1:, 1:, :]) / 2.0
-    ur_grid[-1, :, :] = ur_grid[-2, :, :] + (ur_grid[-2, :, :] - ur_grid[-3, :, :])
-    ur_grid[:, -1, :] = ur_grid[:, -2, :] + (ur_grid[:, -2, :] - ur_grid[:, -3, :])
-    # Upper left
-    ul_grid = np.zeros_like(grid)
-    ul_grid[0:-1, 1:, :] = (grid[0:-1, 1:, :] + grid[1:, :-1, :]) / 2.0
-    ul_grid[-1, :, :] = ul_grid[-2, :, :] + (ul_grid[-2, :, :] - ul_grid[-3, :, :])
-    ul_grid[:, 0, :] = ul_grid[:, 1, :] - (ul_grid[:, 2, :] - ul_grid[:, 1, :])
-    # Lower right
-    lr_grid = np.zeros_like(grid)
-    lr_grid[1:, 0:-1, :] = (grid[1:, 0:-1, :] + grid[:-1, 1:, :]) / 2.0
-    lr_grid[0, :, :] = lr_grid[1, :, :] - (lr_grid[2, :, :] - lr_grid[1, :, :])
-    lr_grid[:, -1, :] = lr_grid[:, -2, :] + (lr_grid[:, -2, :] - lr_grid[:, -3, :])
-    # Lower left
-    ll_grid = np.zeros_like(grid)
-    ll_grid[1:, 1:, :] = (grid[1:, 1:, :] + grid[:-1, :-1, :]) / 2.0
-    ll_grid[0, :, :] = ll_grid[1, :, :] - (ll_grid[2, :, :] - ll_grid[1, :, :])
-    ll_grid[:, 0, :] = ll_grid[:, 1, :] - (ll_grid[:, 2, :] - ll_grid[:, 1, :])
-
-    GridCorners = namedtuple(
-        "GridCorners", ["ur_grid", "ul_grid", "lr_grid", "ll_grid"]
-    )
-
-    return GridCorners(
-        ur_grid=ur_grid, ul_grid=ul_grid, lr_grid=lr_grid, ll_grid=ll_grid
-    )
-
-
-def _calc_grid_corners_for_lower_left_location(grid):
-    """
-
-    Parameters
-    ----------
-    grid : array
-        3D matrix holding x and y grids. Shape of `grid` must be
-        (height, width, 2).
-
-    Returns
-    -------
-
-    namedtuple with the grids for the four corners around the
-    central grid points
-
-    """
-
-    grid = grid.astype("float64")
-
-    if (np.diff(grid[:, :, 0], axis=1) < 0).any():
-        raise ValueError("x values must be ascending along axis 1")
-    if (np.diff(grid[:, :, 1], axis=0) < 0).any():
-        raise ValueError("y values must be ascending along axis 0")
-
-    # Upper right
-    ur_grid = np.zeros_like(grid)
-    ur_grid[0:-1, 0:-1, :] = grid[1:, 1:, :]
-    ur_grid[-1, :, :] = ur_grid[-2, :, :] + (ur_grid[-2, :, :] - ur_grid[-3, :, :])
-    ur_grid[:, -1, :] = ur_grid[:, -2, :] + (ur_grid[:, -2, :] - ur_grid[:, -3, :])
-    # Upper left
-    ul_grid = np.zeros_like(grid)
-    ul_grid[0:-1, 0:-1, :] = grid[1:, 0:-1, :]
-    ul_grid[-1, :, :] = ul_grid[-2, :, :] + (ul_grid[-2, :, :] - ul_grid[-3, :, :])
-    ul_grid[:, -1, :] = ul_grid[:, -2, :] + (ul_grid[:, -2, :] - ul_grid[:, -3, :])
-    # Lower right
-    lr_grid = np.zeros_like(grid)
-    lr_grid[0:-1, 0:-1, :] = grid[0:-1, 1:, :]
-    lr_grid[-1, :, :] = lr_grid[-2, :, :] + (lr_grid[-2, :, :] - lr_grid[-3, :, :])
-    lr_grid[:, -1, :] = lr_grid[:, -2, :] + (lr_grid[:, -2, :] - lr_grid[:, -3, :])
-    # Lower left
-    ll_grid = grid.copy()
-
-    GridCorners = namedtuple(
-        "GridCorners", ["ur_grid", "ul_grid", "lr_grid", "ll_grid"]
-    )
-
-    return GridCorners(
-        ur_grid=ur_grid, ul_grid=ul_grid, lr_grid=lr_grid, ll_grid=ll_grid
-    )
 
 
 @deprecated(

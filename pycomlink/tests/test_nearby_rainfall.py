0504854f0135326372d3cbb443e21e2627efd72a
max <maximilian.graf@kit.edu>
2023-10-30 10:12:45 +0100

    update test to proper pmax behavior

--- pycomlink/tests/test_nearby_rainfall.py
+++ pycomlink/tests/test_nearby_rainfall.py
@@ -14,15 +14,12 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         wet = xr.DataArray(
             np.concatenate([np.repeat(1, 100), np.repeat(0, 100)]),
             coords=dict(time=time))
-        pmin1 = xr.DataArray(np.linspace(0, -10, len(wet)),
-                             coords=dict(time=time))
-        pmin2 = xr.DataArray(np.linspace(0, -30, len(wet)),
+        pmin = xr.DataArray(np.linspace(0, -30, len(wet)),
                              coords=dict(time=time))
         pmax = xr.DataArray(np.linspace(0, 10, len(wet)),
                             coords=dict(time=time))
 
-        res1 = nearby_rain.nearby_determine_reference_level(wet, pmin1)
-        res2 = nearby_rain.nearby_determine_reference_level(wet, pmin2, pmax)
+        res = nearby_rain.nearby_determine_reference_level(pmin, pmax, wet)
 
         result = np.array(
             [np.nan, np.nan, np.nan, np.nan, np.nan,
@@ -67,51 +64,82 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
              -7.4120603, -7.46231156, -7.51256281, -7.56281407, -7.61306533])
 
         np.testing.assert_array_almost_equal(
-            res1.values,
+            res.values,
             result
         )
 
-        np.testing.assert_array_almost_equal(
-            res2.values,
-            result
-        )
 
     def test_nearby_p_correction(self):
         time = pd.date_range("2020-01-01 00:00", periods=100)
-        pmin = xr.DataArray(np.linspace(0, -10, 100),
+        pmin = xr.DataArray(np.linspace(0, -30, 100),
+                            coords=dict(time=time))
+        pmax = xr.DataArray(np.linspace(0, 10, 100),
                             coords=dict(time=time))
         wet = xr.DataArray(
             np.concatenate([np.repeat(0, 50), np.repeat(1, 50)]),
             coords=dict(time=time))
         pref = xr.DataArray(np.repeat(-8, 100),
                             coords=dict(time=time))
-        res_pcmin, res_pcmax = nearby_rain.nearby_correct_recieved_signals(
-            pmin, wet, pref)
+        pcmin, pcmax = nearby_rain.nearby_correct_received_signals(
+            pmin, pmax, wet, pref)
 
-        result = np.array([-8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8., -8., -8., -8., -8., -8., -8., -8.,
-                           -8.08080808, -8.18181818, -8.28282828, -8.38383838,
-                           -8.48484848, -8.58585859, -8.68686869, -8.78787879,
-                           -8.88888889, -8.98989899, -9.09090909, -9.19191919,
-                           -9.29292929, -9.39393939, -9.49494949, -9.5959596,
-                           -9.6969697, -9.7979798, -9.8989899, -10.])
+        res_pcmin  =np.array([
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        ,  -8.        ,  -8.        ,
+            -8.        ,  -8.        , -15.15151515, -15.45454545,
+           -15.75757576, -16.06060606, -16.36363636, -16.66666667,
+           -16.96969697, -17.27272727, -17.57575758, -17.87878788,
+           -18.18181818, -18.48484848, -18.78787879, -19.09090909,
+           -19.39393939, -19.6969697 , -20.        , -20.3030303 ,
+           -20.60606061, -20.90909091, -21.21212121, -21.51515152,
+           -21.81818182, -22.12121212, -22.42424242, -22.72727273,
+           -23.03030303, -23.33333333, -23.63636364, -23.93939394,
+           -24.24242424, -24.54545455, -24.84848485, -25.15151515,
+           -25.45454545, -25.75757576, -26.06060606, -26.36363636,
+           -26.66666667, -26.96969697, -27.27272727, -27.57575758,
+           -27.87878788, -28.18181818, -28.48484848, -28.78787879,
+           -29.09090909, -29.39393939, -29.6969697 , -30.        ])
+        res_pcmax = np.array([
+            -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+           -8.        , -8.        , -8.        , -8.        , -8.        ,
+            5.05050505,  5.15151515,  5.25252525,  5.35353535,  5.45454545,
+            5.55555556,  5.65656566,  5.75757576,  5.85858586,  5.95959596,
+            6.06060606,  6.16161616,  6.26262626,  6.36363636,  6.46464646,
+            6.56565657,  6.66666667,  6.76767677,  6.86868687,  6.96969697,
+            7.07070707,  7.17171717,  7.27272727,  7.37373737,  7.47474747,
+            7.57575758,  7.67676768,  7.77777778,  7.87878788,  7.97979798,
+            8.08080808,  8.18181818,  8.28282828,  8.38383838,  8.48484848,
+            8.58585859,  8.68686869,  8.78787879,  8.88888889,  8.98989899,
+            9.09090909,  9.19191919,  9.29292929,  9.39393939,  9.49494949,
+            9.5959596 ,  9.6969697 ,  9.7979798 ,  9.8989899 , 10.        ])
+
 
         np.testing.assert_array_almost_equal(
-            res_pcmin.values,
-            result
+            pcmin.values,
+            res_pcmin
         )
 
         np.testing.assert_array_almost_equal(
-            res_pcmax.values,
-            result
+            pcmax.values,
+            res_pcmax
         )
 
     def test_rainfall_calc(self):
@@ -119,7 +147,11 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         time = pd.date_range("2020-01-01 00:00", periods=100)
         cml_id = ["cml_1"]
         pmin = xr.DataArray(
-            np.reshape((np.linspace(0, -10, 100)), (1, 100)),
+            np.reshape((np.linspace(0, -30, 100)), (1, 100)),
+            coords=dict(cml_id=cml_id, time=time),
+        )
+        pmax = xr.DataArray(
+            np.reshape((np.linspace(0, 10, 100)), (1, 100)),
             coords=dict(cml_id=cml_id, time=time),
         )
         wet = xr.DataArray(
@@ -137,8 +169,8 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         )
         F[0,20] = -44
 
-        pcmin, pcmax = nearby_rain.nearby_correct_recieved_signals(
-            pmin, wet, pref)
+        pcmin, pcmax = nearby_rain.nearby_correct_received_signals(
+            pmin, pmax, wet, pref)
         length = 5
         f_GHz = xr.DataArray([25])
         pol = xr.DataArray(['Vertical'])
@@ -160,40 +192,44 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         )
 
         result = np.array([[
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-                np.nan, 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.10367216, 0.25556143,
-            0.40242787, 0.54640596, 0.68835262, 0.82873479, 0.96784663,
-            1.1058904 , 1.24301365, 1.37932866, 1.51492374, 1.64987012,
-            1.78422644, 1.9180418 , 2.05135791, 2.18421063, 2.31663111,
-            2.44864666, 2.5802814 , 2.71155681, 2.84249211, 2.97310463,
-            3.10341004, 3.2334226 , 3.36315532, 3.49262013, 3.62182803,
-            3.75078913, 3.87951283, 4.00800783, 4.13628224, 4.26434363,
-            4.39219907, 4.51985519, 4.64731821, 4.774594  , 4.90168807]])
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            np.nan, 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0.,
+            3.70163655, 3.82180766, 3.94177644, 4.06154969, 4.18113378,
+            4.3005347, 4.41975808, 4.5388092, 4.65769307, 4.77641441,
+            4.89497769, 5.01338714, 5.13164677, 5.24976042, 5.36773171,
+            5.4855641, 5.60326089, 5.72082524, 5.83826015, 5.95556851,
+            6.07275307, 6.18981648, 6.30676126, 6.42358986, 6.54030461,
+            6.65690776, 6.77340147, 6.88978782, 7.00606882, 7.12224641,
+            7.23832245, 7.35429874, 7.47017703, 7.58595899, 7.70164626,
+            7.8172404, 7.93274294, 8.04815535, 8.16347907, 8.27871548,
+            8.39386593, 8.50893172, 8.62391412, 8.73881436, 8.85363363,
+            8.9683731, 9.08303389, 9.19761711, 9.31212382, 9.42655507]])
 
         np.testing.assert_array_almost_equal(
             R.values,
             result
         )
 
-        # with give a and b values and with a channel id
+        # with given a and b values and with a channel id
 
         time = pd.date_range("2020-01-01 00:00", periods=100)
         cml_id = ["cml_1"]
         channel_id = ["channel_1"]
         pmin = xr.DataArray(
-            np.reshape((np.linspace(0, -10, 100)), (1, 1, 100)),
-            coords=dict(cml_id=cml_id, channel_id=channel_id, time=time),
+            np.reshape((np.linspace(0, -30, 100)), (1, 100)),
+            coords=dict(cml_id=cml_id, time=time),
+        )
+        pmax = xr.DataArray(
+            np.reshape((np.linspace(0, 10, 100)), (1, 100)),
+            coords=dict(cml_id=cml_id, time=time),
         )
         wet = xr.DataArray(
             np.reshape(np.concatenate([np.repeat(0, 50), np.repeat(1, 50)]),
@@ -210,11 +246,9 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         )
         F[0, 0, 20] = -44
 
-        pcmin, pcmax = nearby_rain.nearby_correct_recieved_signals(
-            pmin, wet, pref)
+        pcmin, pcmax = nearby_rain.nearby_correct_received_signals(
+            pmin, pmax, wet, pref)
         length = 5
-        f_GHz = xr.DataArray([25])
-        pol = xr.DataArray(['Vertical'])
 
 
         R = nearby_rain.nearby_rainfall_retrival(
@@ -232,26 +266,19 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         )
 
         result = np.array([[[
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-                np.nan, 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.        , 0.        ,
-            0.        , 0.        , 0.        , 0.01272727, 0.03292929,
-            0.05313131, 0.07333333, 0.09353535, 0.11373737, 0.13393939,
-            0.15414141, 0.17434343, 0.19454545, 0.21474747, 0.23494949,
-            0.25515152, 0.27535354, 0.29555556, 0.31575758, 0.3359596 ,
-            0.35616162, 0.37636364, 0.39656566, 0.41676768, 0.4369697 ,
-            0.45717172, 0.47737374, 0.49757576, 0.51777778, 0.5379798 ,
-            0.55818182, 0.57838384, 0.59858586, 0.61878788, 0.6389899 ,
-            0.65919192, 0.67939394, 0.69959596, 0.71979798, 0.74      ]]])
+            0., 0., 0., 0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0., 0., 0., 0.,
+            0., 0., 0., 0., np.nan, 0., 0., 0.,
+            0., 0., 0., 0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0., 0., 0., 0.,
+            0., 0., 0., 0., 0., 0., 0., 0.,
+            0., 0., 0.5842, 0.6042, 0.6242, 0.6442, 0.6642, 0.6842,
+            0.7042, 0.7242, 0.7442, 0.7642, 0.7842, 0.8042, 0.8242, 0.8442,
+            0.8642, 0.8842, 0.9042, 0.9242, 0.9442, 0.9642, 0.9842, 1.0042,
+            1.0242, 1.0442, 1.0642, 1.0842, 1.1042, 1.1242, 1.1442, 1.1642,
+            1.1842, 1.2042, 1.2242, 1.2442, 1.2642, 1.2842, 1.3042, 1.3242,
+            1.3442, 1.3642, 1.3842, 1.4042, 1.4242, 1.4442, 1.4642, 1.4842,
+            1.5042, 1.5242, 1.5442, 1.5642]]])
 
         np.testing.assert_array_almost_equal(
             R.values,
@@ -262,7 +289,11 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         time = pd.date_range("2020-01-01 00:00", periods=100)
         cml_id = ["cml_1"]
         pmin = xr.DataArray(
-            np.reshape((np.linspace(0, -10, 100)), (1, 100)),
+            np.reshape((np.linspace(0, -30, 100)), (1, 100)),
+            coords=dict(cml_id=cml_id, time=time),
+        )
+        pmax = xr.DataArray(
+            np.reshape((np.linspace(0, 10, 100)), (1, 100)),
             coords=dict(cml_id=cml_id, time=time),
         )
         wet = xr.DataArray(
@@ -280,8 +311,8 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
         )
         F[0, 20] = -44
 
-        pcmin, pcmax = nearby_rain.nearby_correct_recieved_signals(
-            pmin, wet, pref)
+        pcmin, pcmax = nearby_rain.nearby_correct_received_signals(
+            pmin, pmax, wet, pref)
         length = 5
         f_GHz = xr.DataArray([25])
         pol = xr.DataArray(['Vertical'])
@@ -295,12 +326,13 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
                 F=F,
                 length=length,
                 f_GHz=f_GHz,
+                pol=pol,
                 a=None,
                 b=1,
                 a_b_approximation="ITU_2005",
                 waa_max=2.3,
                 alpha=0.33,
-                F_value_correction=True
+                F_value_correction=True,
             )
 
         with self.assertRaises(IndexError):
@@ -319,23 +351,3 @@ class Test_nearby_wetdry_approach(unittest.TestCase):
                 alpha = 0.33,
                 F_value_correction = True
             )
-
-        R = nearby_rain.nearby_rainfall_retrival(
-            pref=pref,
-            p_c_min=pcmin,
-            p_c_max=pcmax,
-            F=F,
-            length=length,
-            a = 0.5,
-            b = 2,
-            a_b_approximation = "ITU_2005",
-            waa_max = 2.3,
-            alpha = 0.33,
-            F_value_correction = True
-        )
-
-#with self.assertRaises(ValueError):
-#    k_R_relation.a_b(30, "b", approx_type="ITU_2005")
-
-#with self.assertRaises(ValueError):
-#    k_R_relation.a_b(30, "H", approx_type="ITU_2000")

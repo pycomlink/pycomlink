1501c3d1560ffe92ea6483e646121a976b388f62
max <maximilian.graf@kit.edu>
2023-11-08 11:08:23 +0100

    implement review requests: mainly docstring enhancements, adding addtional kwargs and fixing an error in p_c_max calculation as well as adopting tests

--- pycomlink/processing/nearby_rain_retrival.py
+++ pycomlink/processing/nearby_rain_retrival.py
@@ -3,29 +3,45 @@ import xarray as xr
 from pycomlink.processing.k_R_relation import a_b
 
 
-def nearby_determine_reference_level(pmin, pmax, wet, n_average_dry=96):
+def nearby_determine_reference_level(
+        pmin,
+        pmax,
+        wet,
+        n_average_dry=96,
+        min_periods=1):
     """
     Determine reference/baseline level during rain events as Overeem et al.
     (2016). The baseline ist the median of all dry time steps during the last
     `n_average_dry` dry time steps.
+
+    Parameters
     ----------
     pmin : xarray.DataArray
          Time series of pmin.
     pmax : xarray.DataArray
         Time series of pmax. If not available e.g. because the min-max data
         is derived from instantaneous sampled CML data and has the same
-        temporal resolution as the instantaneous CML data, substitute pmax
-        with pmin so pmin and pmax are identical.
+        temporal resolution as the instantaneous CML data, then substitute pmax
+        with pmin so pmin and pmax are identical. This is not optimal because
+        the rainfall estimation in nearby_rainfall_retrival() uses a weighted
+        average between the rain rate derived from pmin and pmax. Using
+        pmin to substitute pmax can lead to overestimation of rainfall.
+
     wet : xarray.DataArray
          DataArray consisting of time series with wet-dry classification.
     n_average_dry: int
         Number of timesteps which are used to calculate the reference levek
         (baseline) from.
+    min_periods: int
+        Number of periods which have to be available in the last n_average_dry
+        time steps to calculate a reference level.
+
 
     Returns
     -------
     pref : xarray.DataArray
         Reference signal level, also called baseline
+
     References
     ----------
     .. [1] Overeem, A., Leijnse, H., and Uijlenhoet, R.: Retrieval algorithm
@@ -38,15 +54,22 @@ def nearby_determine_reference_level(pmin, pmax, wet, n_average_dry=96):
 
     pmean = ((pmin + pmax) / 2).where(dry == 1)
 
-    pref = pmean.rolling(time=n_average_dry, min_periods=1).median()
+    pref = pmean.rolling(time=n_average_dry, min_periods=min_periods).median()
 
     return pref
 
 
 def nearby_correct_received_signals(pmin, pmax, wet, pref):
     """
-    Correcting pmin and pmax to prevent rainfall estimation during dry time
-    steps.
+    Correcting pmin and pmax so that no rainfall estimation is carried out
+    during dry time steps. All time steps of pmin which are not classified wet
+    and pmin is smaller than pref are set to pref. Similarly, all time steps of
+    pmax where either the corrected pmin (p_c_min) is not smaller than pref or
+    pmax is not smaller than pref are set to pref. This ensures that only wet
+    time steps are used for rainfall estimation an and that pmax is not above
+    pref  which would lead to an overestimation of rainfall.
+
+    Parameter
     ----------
     pmin : xarray.DataArray
          Time series of pmin.
@@ -59,12 +82,14 @@ def nearby_correct_received_signals(pmin, pmax, wet, pref):
          DataArray consisting of time series with wet-dry classification.
     pref : xarray.DataArray
         Reference signal level, sometimes also called baseline
+
     Returns
     -------
     p_c_min : xarray.DataArray
         Corrected pmin
     p_c_max : xarray.DataArray
         Corrected pmax, if no pmax is given, p_c_max is identical to p_c_min
+
     References
     ----------
     .. [1] Overeem, A., Leijnse, H., and Uijlenhoet, R.: Retrieval algorithm
@@ -74,8 +99,7 @@ def nearby_correct_received_signals(pmin, pmax, wet, pref):
     """
 
     p_c_min = xr.where(cond=(pmin < pref) & (wet == 1), x=pmin, y=pref)
-
-    p_c_max = xr.where(cond=(p_c_min < pref) & (pmin < pref), x=pmax, y=pref)
+    p_c_max = xr.where(cond=(p_c_min < pref) & (pmax < pref), x=pmax, y=pref)
 
     return p_c_min, p_c_max
 
@@ -93,14 +117,21 @@ def nearby_rainfall_retrival(
     a_b_approximation="ITU_2005",
     waa_max=2.3,
     alpha=0.33,
-    F_value_correction=True,
+    F_value=-32.5,
 ):
     """
-    Calculating R from corrected pmin and pmax values using the `k-R`-relation.
+    Calculating R from corrected pmin and pmax values using the `k-R`-relation
+    using a and b values from ITU tables (1), (2). Please note that these values
+    are not the same values as used in (3) Overeem et al. 2016 who derived
+    a and b by fitting a R-k relation to their DSD data. Therefore, there is a
+    deviation between a_overeem and 1/a_ITU if b is not equal to 1.
+    deviation between a_overeem and 1/a_ITU if b is not equal to 1.
     Wet antenna is derived via Schleiss et al. (2010), the factor `alpha`
     determines the contribution of the minimum and maximum path-averaged
-    rainfall intensity to the returend rain rate. Optionally, the F-Score
+    rainfall intensity to the returend rain rate. The F-Score
     derived from `nearby_wetdry()` can be used to remove outliers.
+
+    Parameters
     ----------
     pref : xr.DataArray
        Reference signal level, sometimes also called baseline
@@ -118,9 +149,13 @@ def nearby_rainfall_retrival(
         to be provided together with `f_GHz`. It will be used to derive the
         parameters a and b for the k-R power law.
     a : float, optional
-        Parameter of A-R relationship
+        Parameter of k-R relationship which can be taken from ITU (1) or (2).
+        Note that it is not equal to 1/a used in Overeem et al. 2016 who used a
+        R-k relation and own DSD data to derive values for a and b.
     b : float, optional
-        Parameter of A-R relationship
+        Parameter of k-R relationship which can be taken from ITU (1) or (2).
+        Note that it is not equal to 1/a used in Overeem et al. 2016 who used a
+        R-k relation and own DSD data to derive values for a and b.
     a_b_approximation : string
         Specifies which approximation for the k-R power law shall be used. See
         the function `a_b` for details.
@@ -128,8 +163,28 @@ def nearby_rainfall_retrival(
         Maximum value of wet antenna attenuation
     alpha : float
         Between 0 and 1. B
+    F_value: float
+        Outlier detection value calculated in `nearby_wetdry()` can be used to
+        remove outliers. Set to `None` if it should not be used.
 
-    F_values_correction=True
+    Returns
+    ----------
+    R: xr.DataArray
+        Rain rate in mm/h.
+
+    References:
+    ----------
+    (1) & (1) Specific attenuation model for rain for use in prediction
+    methods. Geneva, Switzerland: ITU-R.
+    Versions within pycomlink:
+    (1) P.838-2 (04/2003)
+    (2) P.838-3 (03/2005)
+    Retrieved from https://www.itu.int/rec/R-REC-P.838
+
+    (2) Overeem, A., Leijnse, H., & Uijlenhoet, R. (2016). Retrieval algorithm
+    for rainfall mapping from microwave links in a cellular communication
+    network. Atmospheric Measurement Techniques, 9(5), 2425–2444.
+    https://doi.org/10.5194/amt-9-2425-2016
     """
 
     # Make sure that we only continue if a correct combination of optional
@@ -188,14 +243,14 @@ def nearby_rainfall_retrival(
     r_max = ((A_max - waa_max)/(a*length))**(1/b)
 
     # not allowing negative rain intensities
-    r_min = xr.where(A_min - waa_max < 0, 0, r_min)
-    r_max = xr.where(A_max - waa_max < 0, 0, r_max)
+    r_min = xr.where(r_min < 0, 0, r_min)
+    r_max = xr.where(r_max < 0, 0, r_max)
 
     # weighted mean path averaged rainfall intensity
     R = (alpha * r_max) + ((1 - alpha) * r_min)
 
     # correct rainfall intensities by removing outliers defined by the F score
-    if F_value_correction is True:
-        R = R.where(~(F <= -32.5))
+    if F_value is not None:
+        R = R.where(~(F <= F_value))
 
     return R

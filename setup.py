66a755ee43590fd0dae6636f2329f15dc3e2e4eb
Christian Chwala <christian.chwala@kit.edu>
2025-10-09 13:19:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name = "pycomlink",
-    version = "0.4.1",
+    version = "0.5.0",
     author = "Christian Chwala",
     author_email = "christian.chwala@kit.edu",
     description = ("Python tools for CML (commercial microwave link) data processing"),
@@ -28,7 +28,7 @@ setup(
     keywords = "microwave links precipitation radar cml",
     url = "https://github.com/pycomlink/pycomlink",
     download_url = (
-        "https://github.com/pycomlink/pycomlink/archive/0.4.1.tar.gz"),
+        "https://github.com/pycomlink/pycomlink/archive/0.5.0.tar.gz"),
     packages=find_packages(exclude=['test']),
     include_package_data=True,
     long_description=read('README.md'),

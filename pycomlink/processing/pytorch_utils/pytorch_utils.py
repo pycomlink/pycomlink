7bac14dcd3fbaca1353ae54858e077a8fed5f428
julius.polz <julius.polz@kit.edu>
2025-11-11 09:49:21 +0100

    add reshape option

--- pycomlink/processing/pytorch_utils/pytorch_utils.py
+++ pycomlink/processing/pytorch_utils/pytorch_utils.py
@@ -51,7 +51,7 @@ def predict_batch(model, batch, device):
 
 
 # Build PyTorch DataLoader
-def build_dataloader(combined_samples, batch_size):
+def build_dataloader(combined_samples, batch_size, reshape=False):
     """
     Builds a PyTorch DataLoader from the input data.
 
@@ -59,15 +59,18 @@ def build_dataloader(combined_samples, batch_size):
         data (xarray.DataArray): Batched input data array.
         window_size (int): The size of each time series window.
         batch_size (int): The number of samples in each batch.
-        device (torch.device): The device to run the model on.
-        reflength (int): The reference length for timestamp calculation (from config).
+        reshape (bool or tuple): Whether to reshape the input before passing to the model (default: False).
 
     Returns:
         dataloader (torch.utils.data.DataLoader): A DataLoader for the input data.
     """
     # Only batch the data tensor; keep cml_id and time as arrays outside the DataLoader
     tensor_data = torch.tensor(combined_samples["data"], dtype=torch.float32)
-    tensor_data = tensor_data.permute(0, 2, 1)  # (batch, channels, window)
+    if reshape:
+        permute = reshape
+    else:
+        permute = (0, 2, 1)
+    tensor_data = tensor_data.permute(*permute)  # (batch, channels, window)
 
     dataloader = torch.utils.data.DataLoader(
         tensor_data, batch_size=batch_size, shuffle=False

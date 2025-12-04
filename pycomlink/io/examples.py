79754d9c02effdbb65e001022712fe4de9fb5e69
Maximilian Graf <maximilian.graf@kit.edu>
2025-12-04 17:38:46 +0100

    update importlib usage

--- pycomlink/io/examples.py
+++ pycomlink/io/examples.py
@@ -5,7 +5,9 @@ from .. io.cmlh5_to_xarray import read_cmlh5_file_to_xarray
 
 
 def get_example_data_path():
-    return str(resources.files("pycomlink.io").joinpath("example_data"))
+    examples = resources.files("pycomlink.io").joinpath("example_data")
+    with resources.as_file(examples) as real_path:
+        return str(real_path)
 
 
 def get_one_cml():
@@ -24,3 +26,7 @@ def get_75_cmls_min_max():
     data_path = get_example_data_path()
     fn = "75_cmls_min_max.h5"
     return read_cmlh5_file_to_xarray(path.join(data_path, fn))
+
+
+import importlib.resources as resources
+

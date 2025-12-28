5c6d04cd760080d6f5f2b5d331d30473f194022e
max <maximilian.graf@kit.edu>
2025-12-28 12:26:37 +0100

    drop python 3.9 from CI

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
         "License :: OSI Approved :: BSD License",
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
